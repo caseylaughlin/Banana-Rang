b4d75d3ea7a84bee5ccebfa98a0c4cd2e48ca1bb
Casey.Laughlin <caseylaughlin1@me.com>
2015-10-29 13:39:47 -0600

    Fixed

--- script.js
+++ script.js
@@ -1,229 +1,229 @@
-                    var canvas;
-                    var ctx;
-                    var mx = 3;
-                    var my = 1;
-                    var screenWidth = 700;
-                    var screenHeight = 350;
-                    var banannaWidth = 50;
-                    var banannaHeight = 50;
-                    var monkeyWidth = 50;
-                    var monkeyHeight = 50;
-                    var monkeyPosX = 25;
-                    var monkeyPosY = 125;
-                    var monkeySpeed = 5;
-                    var x = monkeyPosX + banannaWidth;        // this is the starting position
-                    var y = monkeyPosY;
-                    var held = true;
-                    var evilMonkeyHeight = 50;
-                    var evilMonkeyWidth = 50;
+var canvas;
+var ctx;
+var mx = 3;
+var my = 1;
+var screenWidth = 700;
+var screenHeight = 350;
+var banannaWidth = 50;
+var banannaHeight = 50;
+var monkeyWidth = 50;
+var monkeyHeight = 50;
+var monkeyPosX = 25;
+var monkeyPosY = 125;
+var monkeySpeed = 5;
+var x = monkeyPosX + banannaWidth;        // this is the starting position
+var y = monkeyPosY;
+var held = true;
+var evilMonkeyHeight = 50;
+var evilMonkeyWidth = 50;
 var score = 0;
-                    var evilMonkeyPosX = Math.floor((Math.random()*350 - evilMonkeyWidth) + 350);
-                    var evilMonkeyPosY = Math.floor(Math.random()*330 - evilMonkeyHeight) + monkeyHeight;
-                    var evilVisibleMonkey = true;
-                    var hardButton = document.getElementById("hard");
-        
-                    
-                    var bananna = new Image();
-                    bananna.src = "images/bananna.png";
-                    var evilMonkey = new Image();
-                    evilMonkey.src = "images/evilMonkey.png";
+var evilMonkeyPosX = Math.floor((Math.random()*350 - evilMonkeyWidth) + 350);
+var evilMonkeyPosY = Math.floor(Math.random()*330 - evilMonkeyHeight) + monkeyHeight;
+var evilVisibleMonkey = true;
+var hardButton = document.getElementById("hard");
+
+
+var bananna = new Image();
+bananna.src = "images/bananna.png";
+var evilMonkey = new Image();
+evilMonkey.src = "images/evilMonkey.png";
 
 
 
 
 
 function monkeyDorkMonkey()
-{
-   
-    monkey = new Image();
-    monkey.src = "images/dorkMonkey.png";
-    document.getElementById("characterText").style.zIndex = "-2";
-}
+	{
+
+	monkey = new Image();
+	monkey.src = "images/dorkMonkey.png";
+	document.getElementById("characterText").style.zIndex = "-2";
+	}
 function monkeyBuffMonkey()
-{
-    
-    monkey = new Image();
-    monkey.src = "images/buffMonkey.png";
-    document.getElementById("characterText").style.zIndex = "-2";
-}
+	{
+
+	monkey = new Image();
+	monkey.src = "images/buffMonkey.png";
+	document.getElementById("characterText").style.zIndex = "-2";
+	}
 function monkeySportMonkey()
-{
-   
-    monkey = new Image();
-    monkey.src = "images/sportMonkey.jpg";
-    document.getElementById("characterText").style.zIndex = "-2";
-}
-        
-        
-        
-        function drawBorder()
-            {
-            ctx.rect(0,0,screenWidth, screenHeight);
-            ctx.stroke();
-            }
-        function playerMonkey(monkeyPosX, monkeyPosY)
-            {
-            ctx.drawImage(monkey, monkeyPosX, monkeyPosY);
-            }
+	{
+
+	monkey = new Image();
+	monkey.src = "images/sportMonkey.jpg";
+	document.getElementById("characterText").style.zIndex = "-2";
+	}
+
+
+
+function drawBorder()
+	{
+	ctx.rect(0,0,screenWidth, screenHeight);
+	ctx.stroke();
+	}
+function playerMonkey(monkeyPosX, monkeyPosY)
+	{
+	ctx.drawImage(monkey, monkeyPosX, monkeyPosY);
+	}
 
 function moveEvilMonkey(evilMonkeyPosX, evilMonkeyPosY)
-{
-    ctx.drawImage(evilMonkey, evilMonkeyPosX, evilMonkeyPosY)
+	{
+	ctx.drawImage(evilMonkey, evilMonkeyPosX, evilMonkeyPosY)
 
-}
+	}
 
 
-        
-        function banannaS(x, y)   //this draws the square
-            {
-            ctx.drawImage(bananna, x, y);      
-            //outerwalls
-            if (x + mx > screenWidth - banannaWidth || x + mx < 0)
-                mx = -mx;       
-            if (y + my > screenHeight - banannaHeight || y + my < 0)
-                my = -my;
-                
-            //playerMonkey 
-            if (
-                (y + my >= evilMonkeyPosY - banannaHeight &&
-                 y + my <= evilMonkeyPosY + evilMonkeyHeight) &&
-                (x + mx == evilMonkeyPosX - banannaWidth ||
-                 x + mx == evilMonkeyPosX + evilMonkeyWidth))
-                    {
-                    evilMonkeyPosX = Math.floor((Math.random()*350 - evilMonkeyWidth) + 350);
-                    evilMonkeyPosY = Math.floor(Math.random()*310 - evilMonkeyHeight) + evilMonkeyHeight;
-                    mx = -mx;
-                    makeEvilMonkey1();
-                        score++;
-                        document.getElementById("score").innerHTML = "Your score: " + score;
-                    }
-            if (   
-                (y + my >= monkeyPosY - banannaHeight &&  
-                 y + my <= monkeyPosY + monkeyHeight) && 
-                (x + mx == monkeyPosX - banannaWidth ||
-                 x + mx == monkeyPosX + monkeyWidth))
-                    {
-                    held = true;
-                    
-                    mx = -mx;    
-     			    }
-            }
 
-        function clear()        //this clears the canvas
-            {
-            ctx.clearRect(0, 0, screenWidth, screenHeight);
-            }
+function banannaS(x, y)   //this draws the square
+	{
+	ctx.drawImage(bananna, x, y);
+	//outerwalls
+	if (x + mx > screenWidth - banannaWidth || x + mx < 0)
+		mx = -mx;
+	if (y + my > screenHeight - banannaHeight || y + my < 0)
+		my = -my;
 
-        function init()         //this calls the draw() function every 10ms
-        {
-            requestAnimationFrame(init);
-            document.getElementById("hard").style.visibility = "visible";    
-            document.getElementById("button").style.visibility = "hidden";
-            canvas = document.getElementById("myCanvas");
-            ctx = canvas.getContext("2d");
-            return draw();
-            }
-        function init2()
-            {
-            requestAnimationFrame(init2);
-            document.getElementById("button").style.visibility = "visible";
-            document.getElementById("hard").style.visibility = "hidden";
-            canvas = document.getElementById("myCanvas");
-            ctx = canvas.getContext("2d");
-            return draw2();
-            }
-        function draw2()       //this animates the square
-            {
-            clear();
-            drawBorder();
-            playerMonkey(monkeyPosX, monkeyPosY);
-            banannaS(x, y);
-            makeEvilMonkey1();
+	//playerMonkey
+	if (
+		(y + my >= evilMonkeyPosY - banannaHeight &&
+		y + my <= evilMonkeyPosY + evilMonkeyHeight) &&
+		(x + mx == evilMonkeyPosX - banannaWidth ||
+		x + mx == evilMonkeyPosX + evilMonkeyWidth))
+		{
+		evilMonkeyPosX = Math.floor((Math.random()*350 - evilMonkeyWidth) + 350);
+		evilMonkeyPosY = Math.floor(Math.random()*310 - evilMonkeyHeight) + evilMonkeyHeight;
+		mx = -mx;
+		makeEvilMonkey1();
+		score++;
+		document.getElementById("score").innerHTML = "Your score: " + score;
+		}
+	if (
+		(y + my >= monkeyPosY - banannaHeight &&
+		y + my <= monkeyPosY + monkeyHeight) &&
+		(x + mx == monkeyPosX - banannaWidth ||
+		x + mx == monkeyPosX + monkeyWidth))
+		{
+		held = true;
 
-            
-            if(held === false)
-                {
-                x+=mx;
-                }
-                 if(evilMonkeyPosY > screenHeight - evilMonkeyHeight)
-                {
-                    my = -my;
-                }
-                if(evilMonkeyPosY < 0)
-                {
-                    my = -my;
-                }
-                evilMonkeyPosY+=my;
+		mx = -mx;
+		}
+	}
 
-            }
-        function draw()         //this animates the square
-            {
-            clear();
-            drawBorder();
-            playerMonkey(monkeyPosX, monkeyPosY);
-            banannaS(x, y);
-            makeEvilMonkey1();
-            gameChanger();
-            
-            if(held === false)
-                {
-                x+=mx;
-                }
+function clear()        //this clears the canvas
+	{
+	ctx.clearRect(0, 0, screenWidth, screenHeight);
+	}
 
-            }
+function init()         //this calls the draw() function every 10ms
+	{
+	requestAnimationFrame(init);
+	document.getElementById("hard").style.visibility = "visible";
+	document.getElementById("button").style.visibility = "hidden";
+	canvas = document.getElementById("myCanvas");
+	ctx = canvas.getContext("2d");
+	return draw();
+	}
+function init2()
+	{
+	requestAnimationFrame(init2);
+	document.getElementById("button").style.visibility = "visible";
+	document.getElementById("hard").style.visibility = "hidden";
+	canvas = document.getElementById("myCanvas");
+	ctx = canvas.getContext("2d");
+	return draw2();
+	}
+function draw2()       //this animates the square
+	{
+	clear();
+	drawBorder();
+	playerMonkey(monkeyPosX, monkeyPosY);
+	banannaS(x, y);
+	makeEvilMonkey1();
+
+
+	if(held === false)
+		{
+		x+=mx;
+		}
+	if(evilMonkeyPosY > screenHeight - evilMonkeyHeight)
+		{
+		my = -my;
+		}
+	if(evilMonkeyPosY < 0)
+		{
+		my = -my;
+		}
+	evilMonkeyPosY+=my;
+
+	}
+function draw()         //this animates the square
+	{
+	clear();
+	drawBorder();
+	playerMonkey(monkeyPosX, monkeyPosY);
+	banannaS(x, y);
+	makeEvilMonkey1();
+	gameChanger();
+
+	if(held === false)
+		{
+		x+=mx;
+		}
+
+	}
 function gameChanger()
-{
- if(score === 1)
- {
-    document.getElementById("hard").style.visibility = "visible";
-     document.getElementById("levelUnlocked").style.zIndex = "3";
-    document.getElementById("levelUnlocked").style.visibility = "visible";
-     document.getElementById("levelUnlocked").style.visibility = "hidden";
- }
-    
-}
+	{
+	if(score === 1)
+		{
+		document.getElementById("hard").style.visibility = "visible";
+		document.getElementById("levelUnlocked").style.zIndex = "3";
+		document.getElementById("levelUnlocked").style.visibility = "visible";
+		document.getElementById("levelUnlocked").style.visibility = "hidden";
+		}
+
+	}
 function makeEvilMonkey1()
-{
-     moveEvilMonkey(evilMonkeyPosX, evilMonkeyPosY);
-}
-        
-        function doKeyDown(event)
-            {
-                
-                
-            switch (event.keyCode) 
-                {
-                case 38:  /* Up arrow was pressed */
-                    monkeyPosY -= monkeySpeed;
-                    y -= monkeySpeed;
-                        if(y < 0)
-                        {
-                            y = 0;
-                        }
-                    if (monkeyPosY < 0)
-                        {
-                        monkeyPosY = 0;
-                        }
-                    break;
-                case 40:  /* Down arrow was pressed */
-                    monkeyPosY += monkeySpeed;
-                        y += monkeySpeed;
-                        if(y > screenHeight - banannaHeight)
-                        {
-                            y = screenHeight - banannaHeight;
-                        }
-                    if (monkeyPosY > screenHeight - banannaHeight)
-                        {
-                        monkeyPosY = screenHeight - banannaHeight;
-                        }
-                    break;
-                        
-                case 32:
-                        {
-                            held = false;
-                        }
-                    break;
- 
+	{
+	moveEvilMonkey(evilMonkeyPosX, evilMonkeyPosY);
+	}
+
+function doKeyDown(event)
+	{
+
+
+	switch (event.keyCode)
+	{
+		case 38:  /* Up arrow was pressed */
+			monkeyPosY -= monkeySpeed;
+			y -= monkeySpeed;
+			if(y < 0)
+				{
+				y = 0;
+				}
+			if (monkeyPosY < 0)
+				{
+				monkeyPosY = 0;
+				}
+			break;
+		case 40:  /* Down arrow was pressed */
+			monkeyPosY += monkeySpeed;
+			y += monkeySpeed;
+			if(y > screenHeight - banannaHeight)
+				{
+				y = screenHeight - banannaHeight;
+				}
+			if (monkeyPosY > screenHeight - banannaHeight)
+				{
+				monkeyPosY = screenHeight - banannaHeight;
+				}
+			break;
+
+		case 32:
+			{
+			held = false;
+			}
+			break;
+
 //                case 37:  /* Left arrow was pressed */
 //                    monkeyPosX -= monkeySpeed;
 //                    if (monkeyPosX < 0)
@@ -238,8 +238,6 @@ function makeEvilMonkey1()
 //                        monkeyPosX = screenWidth - banannaWidth;
 //                        }
 //                    break;
-                }
-            }
-
-                           //this starts the whole thing going
-        window.addEventListener("keydown", doKeyDown, true);
+	}
+	}
+window.addEventListener("keydown", doKeyDown, true);
